b628bd5bcd653e45483aeb8b075dc829f3a94b1b
Andy Zhang <z-c-ge@outlook.com>
2025-10-28 16:08:48 +0800

    Fix problems for Windows

--- src/stpInterrupt.cpp
+++ src/stpInterrupt.cpp
@@ -7,24 +7,27 @@
 
 namespace steppable::parser
 {
+    static std::function<void(void)> handler;
+#ifdef WINDOWS
+    static BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
+    {
+        if (fdwCtrlType == CTRL_C_EVENT)
+        {
+            if (handler)
+                handler();
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+#endif
+
     void STP_addCtrlCHandler(const std::function<void(void)>& predicate)
     {
 #ifdef WINDOWS
-        static std::function<void(void)> handler;
         handler = predicate;
-        static BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
-        {
-            if (fdwCtrlType == CTRL_C_EVENT)
-            {
-                if (handler)
-                    handler();
-                return TRUE;
-            }
-            return FALSE;
-        }
         SetConsoleCtrlHandler(CtrlHandler, TRUE);
 #else
-        static std::function<void(void)> handler;
         handler = predicate;
         (void)std::signal(SIGINT, [](int) {
             if (handler)
